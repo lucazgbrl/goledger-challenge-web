cc57397ba4bc135a49da221414d7a939f770e3af
Lucas Gabriel <lcs.gabriel39@gmail.com>
2024-12-08 21:38:33 -0300

    updt Playlist Page

--- src/features/Playlist/PlaylistsPage.tsx
+++ src/features/Playlist/PlaylistsPage.tsx
@@ -20,14 +20,15 @@ const PlaylistsPage: React.FC<Props> = ({ playlists }) => {
         <h1 className="text-4xl font-bold text-white">Playlists</h1>
       </header>
 
-      <div className="bg-gradient-to-r from-gray-800 to-black rounded-lg shadow-2xl p-6 max-w-sm mx-auto mb-4 hover:scale-105 transition duration-300">
+      <div className="bg-gradient-to-r from-gray-800 via-gray-900 to-black rounded-lg shadow-2xl p-6 max-w-md mx-auto mb-6 hover:scale-105 transition-transform duration-300">
         <button
           onClick={handleFormToggle}
-          className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-md shadow-lg hover:bg-blue-700 transition duration-300 transform hover:scale-105"
+          className="w-full px-6 py-3 bg-blue-600 text-white font-bold text-lg rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-transform duration-300 transform hover:scale-105"
         >
           New Playlist
         </button>
       </div>
+
 
       {isFormVisible && (
         <div className="mt-6">
